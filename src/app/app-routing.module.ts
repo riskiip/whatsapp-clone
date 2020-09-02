0d75b2e7fcad4874a0ff42ce1f0a095252bcd172
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.iid>
2020-09-02 16:46:49 +0700

    Menambah opsi skipLibCheck: true ke dalam tsconfig.json and login Works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MainContainerComponent } from './components/main-container/main-container.component';
-import { ChatRoomComponent } from './components/main-container/chat-area/chat-room/chat-room.component';
-import { ChatDefaultPageComponent } from './components/main-container/chat-area/chat-default-page/chat-default-page.component';
-import { LoginComponent } from './components/login/login.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainContainerComponent } from "./components/main-container/main-container.component";
+import { ChatRoomComponent } from "./components/main-container/chat-area/chat-room/chat-room.component";
+import { ChatDefaultPageComponent } from "./components/main-container/chat-area/chat-default-page/chat-default-page.component";
+import { LoginComponent } from "./components/login/login.component";
+import { ChatGuard } from "./guards/chat.guard";
 
 const routes: Routes = [
-  {path: '', component: MainContainerComponent, children: [
-    {path: 'room/:id', component: ChatRoomComponent},
-    {path: '', component: ChatDefaultPageComponent}
-  ]},
-  {path: 'login', component: LoginComponent}
+  {
+    path: "",
+    component: MainContainerComponent,
+    children: [
+      { path: "room/:id", component: ChatRoomComponent },
+      { path: "", component: ChatDefaultPageComponent },
+    ],
+    canActivate: [ChatGuard],
+  },
+  { path: "login", component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
