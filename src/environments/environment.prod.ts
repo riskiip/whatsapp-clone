e2adee636f281c52de28ac80ef3f5d49956ab907
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.iid>
2020-09-02 15:50:12 +0700

    Menambah firebaseConfig ke dalam environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCeLNykNjGrUwXjQ0ZphhY9w6K-CrkNh0A",
+    authDomain: "cloningwa.firebaseapp.com",
+    databaseURL: "https://cloningwa.firebaseio.com",
+    projectId: "cloningwa",
+    storageBucket: "cloningwa.appspot.com",
+    messagingSenderId: "202573348622",
+    appId: "1:202573348622:web:6aaf33940a4d10c37d615f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCeLNykNjGrUwXjQ0ZphhY9w6K-CrkNh0A",
+    authDomain: "cloningwa.firebaseapp.com",
+    databaseURL: "https://cloningwa.firebaseio.com",
+    projectId: "cloningwa",
+    storageBucket: "cloningwa.appspot.com",
+    messagingSenderId: "202573348622",
+    appId: "1:202573348622:web:6aaf33940a4d10c37d615f"
+  }
 };
 
 /*
