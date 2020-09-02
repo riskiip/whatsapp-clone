ed64d81f6224512d35e25612bcdf64aee609d11c
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.iid>
2020-09-02 16:37:04 +0700

    Selesai mengatur guard

--- src/app/guards/chat.guard.ts
+++ src/app/guards/chat.guard.ts
@@ -1,15 +1,39 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from "@angular/router";
+import { Observable, pipe } from "rxjs";
+import { map, tap } from "rxjs/operators";
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChatGuard implements CanActivate {
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.afAuth.authState.pipe(
+      map((user) => user !== null),
+      tap((value) => {
+        if (!value) {
+          this.router.navigateByUrl("/login").then();
+          return value;
+        } else {
+          return value;
+        }
+      })
+    );
   }
-  
 }
