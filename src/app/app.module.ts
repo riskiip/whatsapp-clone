a98ade615f12251587c9cd177580d1c9145eb9a6
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.iid>
2020-09-02 09:22:21 +0700

    Menambah component-component yang dibutuhkan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { SideBarContentComponent } from './components/side-bar-content/side-bar-content.component';
+import { ChatAreaComponent } from './components/chat-area/chat-area.component';
+import { ChatDefaultPageComponent } from './components/chat-default-page/chat-default-page.component';
+import { ChatRoomComponent } from './components/chat-room/chat-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    SideBarComponent,
+    SideBarContentComponent,
+    ChatAreaComponent,
+    ChatDefaultPageComponent,
+    ChatRoomComponent
   ],
   imports: [
     BrowserModule,
