f2e490edbb0bec65c389187b0c2fb17a3876a6b8
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.iid>
2020-09-02 10:12:24 +0700

    Mengurus routing awal untuk Main Container dan Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { ChatRoomComponent } from './components/main-container/chat-area/chat-room/chat-room.component';
+import { ChatDefaultPageComponent } from './components/main-container/chat-area/chat-default-page/chat-default-page.component';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainContainerComponent, children: [
+    {path: 'room/:id', component: ChatRoomComponent},
+    {path: '', component: ChatDefaultPageComponent}
+  ]},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
