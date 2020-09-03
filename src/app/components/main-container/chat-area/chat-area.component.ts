dd56244a64285c8ae608544aef44bfd329aa3087
riskiip <muhamadrizki19@student.undip.ac.id>
2020-09-03 14:26:11 +0700

    Sudah bisa send message dan ditampilkan

--- src/app/components/main-container/chat-area/chat-area.component.ts
+++ src/app/components/main-container/chat-area/chat-area.component.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { CommonService } from 'src/app/services/common.service';
+import { Subscription } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-chat-area',
   templateUrl: './chat-area.component.html',
-  styleUrls: ['./chat-area.component.scss']
+  styleUrls: ['./chat-area.component.scss'],
 })
 export class ChatAreaComponent implements OnInit {
   @Input() randomSeed: string;
-  constructor() { }
+  subs: Subscription;
+  paramValue: string;
+
+  constructor(
+    private commonService: CommonService,
+    private afs: AngularFirestore
+  ) {}
 
   ngOnInit() {
+    this.subs = this.commonService.pathParam.subscribe((value) => {
+      this.paramValue = value;
+      console.log(this.paramValue);
+    });
   }
 
   onFormSubmitted(form: NgForm) {
-    console.log('bisa');
-  }
+    if (form.invalid) {
+      return;
+    }
 
+    const { message } = form.value;
+    form.resetForm();
+
+    this.afs
+      .collection('rooms')
+      .doc(this.paramValue)
+      .collection('messages')
+      .add({
+        message,
+        user_id: this.commonService.getUser().uid,
+        name: this.commonService.getUser().displayName,
+        time: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+  }
 }
--- src/app/components/main-container/chat-area/chat-room/chat-room.component.ts
+++ src/app/components/main-container/chat-area/chat-room/chat-room.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
-  styleUrls: ['./chat-room.component.scss']
+  styleUrls: ['./chat-room.component.scss'],
 })
-export class ChatRoomComponent implements OnInit {
+export class ChatRoomComponent implements OnInit, OnDestroy {
+  subs: Subscription[] = [];
+  isUser: User;
+  item;
+  messageData: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private commonService: CommonService,
+    private route: ActivatedRoute,
+    private afs: AngularFirestore
+  ) {
+    this.isUser = JSON.parse(localStorage.getItem('user'));
   }
 
+  ngOnInit() {
+    this.subs.push(
+      this.route.paramMap
+        .pipe(map((paramMap) => paramMap.get('id')))
+        .subscribe((routePathParam) =>
+          this.commonService.updatePathParamState(routePathParam)
+        )
+    );
+
+    this.subs.push(
+      this.route.params.subscribe((par) => {
+        // To get all data matching the document id
+        this.afs
+          .collection('rooms')
+          .doc(par.id)
+          .get()
+          .subscribe((data) => {
+            this.item = data;
+          });
+
+        // To get message sorting by time ascendent (order sesuai last message masuk)
+        this.subs.push(
+          this.afs
+            .collection('rooms')
+            .doc(par.id)
+            .collection('messages', (ref) => ref.orderBy('time', 'asc'))
+            .valueChanges()
+            .subscribe((messages) => {
+              this.messageData = messages;
+            })
+        );
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.map((s) => s.unsubscribe());
+  }
 }
